afe8c933acaba04c738c94f3b541d057dc252672
Matthew Li <matthewli2003@gmail.com>
2022-11-19 20:05:17 -0800

    Removed res.header

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ app.get("/", (req, res) => {
     axios.get("https://quizapi.io/api/v1/questions?apiKey=GqUHpV0I96SwSw9eEHOll244Azgj8dLWdE2Oti4r&limit=10")
     .then(response => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
         res.header("Content-Type", "application/json");
         res.send(response.data); 
     })
